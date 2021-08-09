1ff4ae3802f040d48c48ca613af31a0719dc2844
Steve McGrath <smcgrath@tenable.com>
2021-08-09 11:47:19 -0500

    Updated iterator to stop after 100 pages and warn when it runs into larger datasets

--- sectrails/iterators.py
+++ sectrails/iterators.py
@@ -5,6 +5,7 @@ Iterators
 .. autoclass:: PageIterator
     :members:
 '''
+import warnings
 from restfly.iterator import APIIterator
 from copy import copy
 
@@ -25,15 +26,28 @@ class PageIterator(APIIterator):
     _payload = None
     _method = 'GET'
 
-
     def _get_page(self):
         params = copy(self._params)
         params['page'] = self.num_pages + 1
+
+        # if we are about to request any pages over 100, then we need to
+        # terminate the iterator.
+        if params['page'] > 100:
+            warnings.warn('Total results exceeded 100 pages.')
+            raise StopIteration()
+
+        # make the API call
         resp = self._api._req(self._method,
                               self._path,
                               json=self._payload,
                               params=params
                               ).json()
+
+        # update the record counts, page counts, and load the next page of
+        # content into the page list.
         self.total = resp['record_count']
         self.page = resp['records']
         self.page_count += 1
+        if self.total > 100000:
+            warnings.warn(('The API limits the results to the first 100,000 '
+                           'of the {self.total} records available.'))
