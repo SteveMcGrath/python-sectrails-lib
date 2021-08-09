3f8d69064fbfe58bdda058c6ef994dc762e75134
Steve McGrath <smcgrath@tenable.com>
2021-08-09 11:47:50 -0500

    switched ipaddress to pageiterator

--- sectrails/ipaddress.py
+++ sectrails/ipaddress.py
@@ -7,26 +7,39 @@ IP Address API Endpoints
     :members:
 '''
 from restfly.endpoint import APIEndpoint
-from typing import Optional, Literal
+from typing import Optional
+from .iterators import PageIterator
 
 
 class IpAddressesAPI(APIEndpoint):
     _path = 'ips'
 
     def neighbors(self, address: str) -> dict:
+        '''
+        '''
         return self._get(f'nearby/{address}').json()
 
-    def search(self, query: str, page: Optional[int] = None) -> dict:
-        return self._post('list',
-                          params={'page': page},
-                          json={'query': query}
-                          ).json()
+    def search(self, query: str, page: Optional[int] = None) -> PageIterator:
+        '''
+        '''
+        return PageIterator(
+            self._api,
+            _method='POST',
+            _payload={'query': query},
+            _path='ips/list'
+        )
 
     def stats(self, query: str) -> dict:
+        '''
+        '''
         return self._post('stats', json={'query': query}).json()
 
     def whois(self, address: str) -> dict:
+        '''
+        '''
         return self._get(f'{address}/whois').json()
 
-    def user_agents(self, address: str, page: Optional[int] = None) -> dict:
-        return self._get(f'{address}/useragents', params={'page': page}).json()
+    def user_agents(self, address: str) -> PageIterator:
+        '''
+        '''
+        return PageIterator(self._api, _path=f'{address}/useragents')
